f039e74c26ee28fa33cdd250d0d6d3be997e8828
Matheus de Alencar <matheusdealencar.contato@gmail.com>
2025-02-06 22:11:57 -0300

    first commit

---
+++ c06-class/src/Class0.java
@@ -0,0 +1,13 @@
+// Aula 0 de Programação Orientada a Objeto
+
+public class Class0 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("I never has beed worked with Java. That's gonna be a real challange for me.");
+        System.out.println("\nCoursing Software Engineer in:");
+        for (int i = 3; i >0; i--) {
+            System.out.println(i);
+        }
+        System.out.println("\nGo!");
+    }
+}
